4a72dd92b28cf1cd943fc165692849da86fb7c44
kangind <70737516+kangind@users.noreply.github.com>
2020-12-08 10:36:51 +0400

    Add files via upload

---
+++ Arifmetica/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Arifmetica
+{
+    public class Class1
+    {
+        int a = 2;
+        int b = 1;
+
+        public void Plus(int a, int b)
+        {
+            int c = a + b;
+        }
+
+        public void Minus(int a, int b)
+        {
+            int d = a - b;
+        }
+
+        public void Delenie(int a, int b)
+        {
+            int e = a / b;
+        }
+
+        public void Umnosh(int a, int b)
+        {
+            int f = a * b;
+        }
+    }
+}
---
+++ BibliotekaMetodov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BibliotekaMetodov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
